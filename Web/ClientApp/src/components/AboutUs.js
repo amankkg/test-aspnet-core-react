74ed484c8aef0127c15d4746a438e9e6f8e7b52d
amankkg <amankubanychbek@gmail.com>
2021-01-11 21:46:01 +0600

    Add project files.

---
+++ Web/ClientApp/src/components/AboutUs.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export const AboutUs = (props = {}) => {
+    return (
+        <div>
+            <h1>About Us page</h1>
+            <p>This is our first custom page in React + ASP.NET Core app</p>
+        </div>
+    );
+}
